91375e9166172b35800645ce4f9694fe05943676
yahoo786 <126882954+yahoo786@users.noreply.github.com>
2025-12-05 22:18:25 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,8 @@
+a= int(input("enter num1:" ))
+b= int(input("enter num2:" ))
+c= int(input("enter num3:" ))
+d= int(input("enter num4:" ))
+
+mean = (a + b + c + d) / 4
+
+print("Mean is:", mean)
---
+++ bmi.py
@@ -0,0 +1,6 @@
+hight = float(input("enter your hight in meters:"))
+weight = float(input("enter your weight in kg:"))
+
+bmi = weight / (hight*hight)
+
+print ("your bmi :",bmi)
